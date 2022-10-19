f724526e8472b099bcf5f975151ec3e8c7bd2489
Osama saleem <osamasaleem515@gmail.com>
2022-10-19 18:11:30 +0500

    Edited signup component

--- frontend/src/components/signup.js
+++ frontend/src/components/signup.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import {Link, Navigate} from 'react-router-dom'
-import {connect, useDispatch, useSelector} from 'react-redux'
+import {connect} from 'react-redux'
 
 import styled from "styled-components";
 import axios from "axios";
