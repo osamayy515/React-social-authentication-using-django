393172c0ff3e01b5fd2368d61564c67e28c63221
Osama saleem <osamasaleem515@gmail.com>
2022-10-19 18:10:30 +0500

    Edited navbar component

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -8,7 +8,7 @@ const Navbar = ({isAuthenticated, logout}) => {
 
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">The Auth</a>
+                <a className="navbar-brand" href="/">The Auth</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
