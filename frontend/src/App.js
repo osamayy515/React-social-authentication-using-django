1e68342a81398e8fd37e98e210cf1734a3ef5133
Osama saleem <osamasaleem515@gmail.com>
2022-10-19 18:24:53 +0500

    Edited app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 /* import logo from './logo.svg';*/
 /* import './App.css'; */
 import React from 'react'
-import {BrowserRouter, Route, Routes} from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./components/home";
 import Welcome from "./components/welcome";
 import Signup from "./components/signup";
 import Login from "./components/login";
 
 import Layout from "./components/layout";
-// import store from "./store";
 
-import Home from "./components/home";
-
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
@@ -18,12 +18,12 @@ function App() {
       <Provider store={store}>
         <BrowserRouter>
           <Layout>
-          <Routes>
-              <Route exact path="/" element={<Welcome />} />
-              <Route exact path="/home" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-          </Routes>
+            <Routes>
+                <Route exact path="/" element={<Welcome />} />
+                <Route exact path="/home" element={<Home />} />
+                <Route exact path="/login" element={<Login />} />
+                <Route exact path="/signup" element={<Signup />} />
+            </Routes>
           </Layout>
         </BrowserRouter>
       </Provider>
